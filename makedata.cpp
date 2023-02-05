c1a07c4ea0255f56ebe573f42bf49fdca651f793
zhongtianrui <106294609+ZhongTianrui@users.noreply.github.com>
2023-02-05 15:29:11 +0800

    none

--- makedata.cpp
+++ makedata.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
-#include <windows.h>
+#include "include/make.hpp"
 using namespace std;
-#define randd(a, b)  int(a) + (int)b * rand() / (RAND_MAX + 1)
 int main() {
 	ios::sync_with_stdio(false);
-	srand(GetTickCount());
+	pre();
 	/*make your data here
 	use cout in output
 	*/
-	cout << randd(1, 1e9) << " " << randd(1, 1e9);
+	cout << randint(1, 1e9) << " " << randint(1, 1e9);
 	return 0;
 }
